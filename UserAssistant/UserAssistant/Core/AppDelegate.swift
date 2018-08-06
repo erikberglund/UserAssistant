713f735a527a4de8643bef37f89c05543d8a3a7a
Erik Berglund <erik.berglund@mac.com>
2018-08-06 10:19:47 +0200

    Empty Project

---
+++ UserAssistant/UserAssistant/Core/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  UserAssistant
+//
+//  Created by Erik Berglund on 2018-08-06.
+//  Copyright © 2018 Erik Berglund. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+}
+
